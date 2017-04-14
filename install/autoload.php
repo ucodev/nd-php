21870600adbcd3fde3b20760cd9187c0b3f9a386
Pedro A. Hortas <pah@ucodev.org>
2017-04-14 21:18:13 +0100

    v0.04g2

--- install/autoload.php
+++ install/autoload.php
@@ -2,6 +2,6 @@
 
 /* Autoload settings */
 $autoload['libraries'] = array('mpdf', 'phpmailer', 'pchart');
-$autoload['extensions'] = array('pagination', 'timezone', 'breadcrumb');
+$autoload['extensions'] = array('image', 'pagination', 'timezone', 'breadcrumb');
 $autoload['models'] = array('ndphp', 'security', 'features', 'access', 'configuration', 'application', 'accounting', 'logging', 'search');
 $autoload['modules'] = array('request', 'response');
